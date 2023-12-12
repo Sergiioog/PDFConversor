62feeb5452435780c3c6f70dbee48f02ca6cdf67
Sergiioog <sergiogargor9@gmail.com>
2023-12-12 21:42:13 +0100

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,46 +15,56 @@ export class AppComponent {
     let docDefinition = {
       content: [
 
+        { text: 'MI PDF CONVERSOR', style: 'header' },
+
         //Texto Normal
-        'Hola soy Sergio Sergiete',
+        'Hola soy Sergio y estoy haciendo un PDF con PDFMake',
 
-        //Texto en grande
-        { text: 'Hola soy Sergio Sergiete', fontSize:20},
+        //Texto con propiedades de styles
+        { text: 'Me llamo Sergio y este es un texto gigante en negrita y en itálica', style:'titulo_principal'},
 
-        //Texto con propiedades del styles
-        { text: 'Esto es el header', style: 'header' },
-
-        //Texto con propiedades del styles
-        { text: 'Texto con margen', margin:50 }, 
+        //Texto con margen
+        { text: 'Esto es un texto con margen', margin: 30 }, 
 
         //{ text: 'Texto alineado', alignment: 'center' },
 
-        
+        // {
+        //   image: 'assets/pug.jpeg',
+        // },
         //Creamos tablas
         {table: {
 
-            headerRows:1, //el encabezado de la tabla se repetirá automáticamente si la tabla se extiende a lo largo de varias páginas. 
-                          //headerRows se establece en 1, lo que significa que la primera fila de la tabla se considerará como el encabezado y se repetirá en cada página si es necesario.
-            widths: ['*','auto',100,'*'],  //* -> Todo el espacio disponible,  tamaño auto y tamaño fijo de 100 puntos
-            body: [
-              ['Uno', 'Dos', 'Tres', 'Cuatro'],
-              [1,   2,   3,   4   ],
-              [{text : 'Valor en bold', bold:true},'Col2','Col3','Col4'],   
-            ]
-         }
+          headerRows:1, //el encabezado de la tabla se repetirá automáticamente si la tabla se extiende a lo largo de varias páginas. 
+                        //headerRows se establece en 1, lo que significa que la primera fila de la tabla se considerará como el encabezado y se repetirá en cada página si es necesario.
+          widths: ['*','auto',100,'*'],  //* -> Todo el espacio disponible,  tamaño auto y tamaño fijo de 100 puntos
+          body: [
+            ['Uno', 'Dos', 'Tres', 'Cuatro'],
+            [1,   2,   3,   4   ],
+            [{text : 'Valor en bold', bold:true},'Col2','Col3','Col4'],   
+          ]
+       }
        },
-    
 
+        //QR
+        { qr: 'https://sergiioog.github.io/' , margin:30 },
+
+      
     ],
     
     //FUERA DE LA VARIABLE PORQUE LE VAMOS A DAR ESTILOS
-        styles: {
-          header: {
-            fontSize: 40,
-            bold: true,
-            // color:'red'
-          }
+      styles: {
+        header: {
+          fontSize: 40,
+          bold: true,
+          // color: 'red'
+        },
+        titulo_principal: {
+          fontSize: 20,
+          italics: true,
+          bold: true
         }
+      }
+    
     }
 
    
